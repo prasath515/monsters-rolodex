1cc66d2f0454f0c0e521762760d1c4c00917c662
prasath515 <padmaprasath197@gmail.com>
2020-12-10 16:37:18 +0530

    deploy-app

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ class App extends Component {
             .catch(err => console.log('found error in Users API'));
     }
 
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value })
+    }
 
     render() {
 
@@ -29,10 +32,10 @@ class App extends Component {
 
         return (
             <div className="App">
-
+                <h1>Monsters Rolodex</h1>
                 <SearchBox              
                     placeholder= 'search monsters'
-                    handleChange= {e => this.setState({ searchField: e.target.value })}
+                    handleChange= {this.handleChange}
                 />
 
                 <CardList 
